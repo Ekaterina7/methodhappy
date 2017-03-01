5540735ec49dbe95b20bfc39ec82d3edb30720ce
Ekaterina7 <mmmkatia@bk.ru>
2017-03-01 08:31:34 +0500

    makesrecursia

---
+++ sschislenia/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sschislenia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+
+        int b = SystemCaLC(a);
+        Console.WriteLine(b);
+        }
+
+        static int  SystemCaLC(int a)
+        {
+            string s = " ";
+            
+            int tempostotok = 0;
+            while (a != 0)
+            {
+                a = a / 2;
+                tempostotok = a % 2;
+                s = Convert.ToString(tempostotok) + s;
+               
+            }
+
+            return Convert.ToInt32(s);
+           
+            
+
+
+        }
+    }
+}
